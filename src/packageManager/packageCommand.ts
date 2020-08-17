2784f05100f84ace3367ad1c70639f18cfc099a2
eyousefifar <eyousefifar@gmail.com>
2020-08-17 22:46:38 +0430

    make package commnad yarn compatible

--- src/packageManager/packageCommand.ts
+++ src/packageManager/packageCommand.ts
@@ -1,42 +1,32 @@
 import { spawn } from "child_process";
-import { packageManagerExists } from "./packageManagerExists";
-import { IParams, TPackageManager } from "./types";
+import { yarnExists } from "./yarnExists";
+import { IParams} from "../types";
 function getParams(args: IParams): string[] {
-  const { mode, packageManager, packageName } = args;
+  const { mode,  packageName } = args;
   if (mode !== "install" && !packageName) throw new Error("should define package name");
 
   if (mode === "remove" && packageName) {
-    const params =
-      packageManager === "npm"
-        ? ["uninstall", packageName]
-        : ["remove", packageName];
-    return params;
+    
+    return ["remove", packageName];;
   } 
   if (mode === "install") return ["install"];
   
   if (mode === "addPackage" && packageName) {
-    const params =
-      packageManager === "npm"
-        ? ["install", packageName, "--save"]
-        : ["add", packageName];
-    return params;
+ 
+    return ["add", packageName];;
   } else {
     throw new Error(`invalid mode: ${mode}`);
   }
 }
 
 export async function packageCommand(args: IParams) {
-  const { packageManager } = args;
-  let pMExists: boolean;
-  try {
-    pMExists = await packageManagerExists(packageManager);
-  } catch (error) {
-    throw error;
-  }
-  if (pMExists) {
+  
+  const yarnCheck =  await yarnExists();
+  
+  if (yarnCheck) {
     const params = getParams(args);
 
-    const installModules = spawn(packageManager, params);
+    const installModules = spawn("yarn", params);
     for await (const data of installModules.stdout) {
       // nothing to do here for now
       // TODO: use the data to make assumptions
