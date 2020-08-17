9608976efaf2fdf2f41f42bd89447e50c8f1d74b
eyousefifar <eyousefifar@gmail.com>
2020-08-17 22:50:23 +0430

    comment out utils types file for removal

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,44 +1,39 @@
-export interface IValidDirectory {
-  isValid: boolean;
-  base: "root" | "packages" | "downTheRoad" | null;
-}
+// export interface IValidDirectory {
+//   isValid: boolean;
+//   base: "root" | "packages" | "downTheRoad" | null;
+// }
 
-export type TLayers =
-  | "entities"
-  | "usecase"
-  | "controllers"
-  | "interface"
-  | "infra"
-  | "global";
+// export type TLayers =
+//   | "entities"
+//   | "usecase"
+//   | "controllers"
+//   | "interface"
+//   | "infra"
+//   | "global";
 
-export type TPackageManager = "yarn" | "npm";
-export interface IPackage {
-  [key: string]: string;
-}
+// export interface IPackage {
+//   [key: string]: string;
+// }
 
-export interface IJsonDB {
-  packageManager: TPackageManager;
-  entities: string[];
-  usecase: string[];
-  controllers: string[];
-  interface: string[];
-  infra: string[];
-  global: string[];
-}
+// // export interface IJsonDB {
+// //   packageManager: TPackageManager;
+// //   entities: string[];
+// //   usecase: string[];
+// //   controllers: string[];
+// //   interface: string[];
+// //   infra: string[];
+// //   global: string[];
+// // }
 
-export interface ISetPackage {
-  status: "alreadyInstalled" | "added" | "conflict";
-  error?: string;
-}
+// // export interface ISetPackage {
+// //   status: "alreadyInstalled" | "added" | "conflict";
+// //   error?: string;
+// // }
 
 
-export interface IParams {
-  mode: "install" | "addPackage"| "remove";
-  packageName?: string;
-  packageManager: TPackageManager;
-}
 
-export interface IPackageExists {
-  status: boolean,
-  layer?: TLayers;
-}
+
+// // export interface IPackageExists {
+// //   status: boolean,
+// //   layer?: TLayers;
+// // }
