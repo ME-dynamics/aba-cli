8b3ad02ef29c6ddd7525b765b11df2d0332a1230
eyousefifar <eyousefifar@gmail.com>
2020-08-17 22:46:57 +0430

    comment out remove package for whole rewrite

--- src/packageManager/removePackage.ts
+++ src/packageManager/removePackage.ts
@@ -1,21 +1,21 @@
 
-import { jsonDB } from './jsonDB';
-import { packageCommand } from './packageCommand'
+// import { jsonDB } from './jsonDB';
+// import { packageCommand } from './packageCommand'
 
-export async function removePackage(packageName: string) {
-    const db = jsonDB();
-    const exists = db.get.packageExists(packageName);
-    if(!exists.status) throw new Error(`${packageName} is not installed`);
-    try {
-        await packageCommand({
-            mode: "remove",
-            packageManager: db.get.packageManager(),
-            packageName
-        });
+// export async function removePackage(packageName: string) {
+//     const db = jsonDB();
+//     const exists = db.get.packageExists(packageName);
+//     if(!exists.status) throw new Error(`${packageName} is not installed`);
+//     try {
+//         await packageCommand({
+//             mode: "remove",
+//             packageManager: db.get.packageManager(),
+//             packageName
+//         });
 
-        if(exists.layer) db.remove(packageName, exists.layer);
+//         if(exists.layer) db.remove(packageName, exists.layer);
         
-    } catch (error) {
-        throw error;
-    }
-}
+//     } catch (error) {
+//         throw error;
+//     }
+// }
