a6c392fb4792ad2cd843d92d14abe1b3b0da85af
eyousefifar <eyousefifar@gmail.com>
2020-08-17 15:59:22 +0430

    replace sqlite with nedb

--- src/db/add.ts
+++ src/db/add.ts
@@ -1,22 +1,33 @@
-import { createHash } from 'crypto';
-import { IBuildAdd, IAdd } from '../types';
+import { createHash } from "crypto";
+import { IDbClient, IAdd, IPackage } from "../types";
 
-
-
-
-
-export function buildAdd(args: IBuildAdd) {
-    const { run } = args;
-    return async function add(info: IAdd) {
-        const { dev, layer, packageName, version } = info;
-        const sha1 = createHash("sha1");
-        const query = `insert into packages (id, name, version, dev, layer)
-        values (?,?,?,?,?)`;
-        const id = sha1.update(packageName).digest("hex");
-        console.log(id);
-        const params = [id, packageName, version, dev, layer];
-        const res = await run(query, params);
-        console.log(res)
-        // TODO: check if package exists
-    }
+export function buildAdd(args: IDbClient) {
+  const { db } = args;
+  return function add(info: IAdd): Promise<IPackage> {
+    return new Promise((resolve, reject) => {
+      const { dev, layer, packageName, version } = info;
+      const sha1 = createHash("sha1");
+      const id = sha1.update(packageName).digest("hex");
+      const doc = {
+        _id: id,
+        packageName,
+        version,
+        dev,
+        layer,
+      };
+      db.insert(doc, (err, newDoc) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({
+            id: newDoc._id,
+            packageName: newDoc.packageName,
+            version: newDoc.version,
+            dev: newDoc.dev,
+            layer: newDoc.layer,
+          });
+        }
+      });
+    });
+  };
 }
