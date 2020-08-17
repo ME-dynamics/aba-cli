57386a51655e493690f4a9a1272eb3e7974cb4b0
eyousefifar <eyousefifar@gmail.com>
2020-08-17 22:49:49 +0430

    removed unused functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 export { downloadTemplate } from "./downloadTemplate";
 export { unzip } from "./unzip";
-export { updatePackageJson } from "./updatePackageJson";
 export { exclusiveFlag } from "./exclusiveFlag";
-export { jsonDB } from "./jsonDB";
 export {switchToRoot } from './switchToRoot';
