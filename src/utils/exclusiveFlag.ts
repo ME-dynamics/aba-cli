a7492212107bd742bc22fa91a78b38f94cbe43ce
eyousefifar <eyousefifar@gmail.com>
2020-08-17 22:49:31 +0430

    fix layers

--- src/utils/exclusiveFlag.ts
+++ src/utils/exclusiveFlag.ts
@@ -1,26 +1,25 @@
-import { TLayers } from "./types";
+import { TLayers } from "../types";
 
 /**
- * a helper tool, for filtering flags in add command, 
- * it will restrict flags to be used together. 
+ * a helper tool, for filtering flags in add command,
+ * it will restrict flags to be used together.
  * you cannot add a package to entity as well as use case for example
  * if you want to which is really not recommended you should do it globally
  */
 
-export function exclusiveFlag(flag: TLayers | "dev") {
-  const flags = [
+export function exclusiveFlag(flag: TLayers) {
+  const flags: TLayers[] = [
     "entities",
-    "usecase",
+    "usecases",
     "controllers",
-    "interface",
-    "infra",
+    "interfaces",
+    "adapters",
     "global",
+    "dev",
+    "schema",
   ];
   const filter = (item: string) => {
     return item !== flag;
-  }
+  };
   return flags.filter(filter);
 }
-
-
-
