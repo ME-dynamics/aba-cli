3483dae60f9b1ab12716f1f10a4a12cae189c733
eyousefifar <eyousefifar@gmail.com>
2020-08-21 15:52:28 +0430

    export newly created types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,14 @@
 export { IValidDirectory } from "./dir";
-export { ISetPackage, TLayers, IParams } from "./packageManager";
+export { ISetPackage, TLayers, IPackageMode } from "./packageManager";
 export { IDbClient, IAdd, IPackage } from "./db";
+export {
+  ICreateLib,
+  IError,
+  ILog,
+  TError,
+  TLog,
+  TLibraries,
+  THttpVerbs,
+  ICreateLayer,
+  TCreateLayers,
+} from "./commandActions";
