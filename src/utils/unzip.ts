6843540690d94b4ec1358131621adc1114e595a7
eyousefifar <eyousefifar@gmail.com>
2020-08-21 15:53:30 +0430

    removed useless try catch

--- src/utils/unzip.ts
+++ src/utils/unzip.ts
@@ -2,16 +2,10 @@ import { join } from "path";
 import { Open } from "unzipper";
 
 export async function unzip(file: Buffer, serviceName: string): Promise<void> {
-  try {
-    const directory = join(process.cwd(), serviceName);
-    const zippedBuf = await Open.buffer(file);
-    await zippedBuf.extract({
-      path: directory,
-      concurrency: 8,
-    });
-  } catch (error) {
-    throw error;
-  }
-
-  return;
+  const directory = join(process.cwd(), serviceName);
+  const zippedBuf = await Open.buffer(file);
+  await zippedBuf.extract({
+    path: directory,
+    concurrency: 8,
+  });
 }
