544c06e5bc9c9df12da8548a2321ef32641ff067
eyousefifar <eyousefifar@gmail.com>
2020-08-22 13:06:26 +0430

    add new commands to create

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,9 +1,19 @@
 import { Command, flags } from "@oclif/command";
-import { createLib } from "../commandActions";
+import { createLib, createLayer } from "../commandActions";
 import { IError, ILog } from "../types";
+import { cli } from "cli-ux";
 export default class Create extends Command {
-  static description = "creates node js clean architecture micro service";
-  static examples = [`$ aba create yourServiceName`];
+  static description =
+    "creates node js clean architecture, nca layers, node libraries and react + react native project";
+  static examples = [
+    `$ aba create service serviceName`,
+    `$ aba create nodelib libName`,
+    `$ aba create rrn rrnName`,
+    `$ aba create entity entityName`,
+    `$ aba create usecase usecaseName`,
+    `$ aba create adapter adapterName`,
+    `$ aba create schema schemaName`,
+  ];
 
   // static flags = {
   //   help: flags.help({ char: "h" }),
@@ -56,7 +66,7 @@ export default class Create extends Command {
         error: this.logErr,
         log: this.logInfo,
       });
-    } else if (mode === "nodelib"){
+    } else if (mode === "nodelib") {
       const serviceMode = "nodelib";
       const serviceName = `${name}`;
       await createLib({
@@ -65,7 +75,7 @@ export default class Create extends Command {
         error: this.logErr,
         log: this.logInfo,
       });
-    } else if (mode === "rrn"){
+    } else if (mode === "rrn") {
       const serviceMode = "rrn";
       const serviceName = `${name}`;
       await createLib({
@@ -75,13 +85,24 @@ export default class Create extends Command {
         log: this.logInfo,
       });
     } else if (mode === "entity") {
-
+      createLayer({
+        error: this.logErr,
+        log: this.logInfo,
+        mode: "entity",
+        name,
+      });
     } else if (mode === "usecase") {
-
+      this.log("will automatically create controller and interface for you");
+      const httpVerb = await cli.prompt('what http method will this usecase respond to?', {required: true, });
+      createLayer({
+        error: this.logErr,
+        log: this.logInfo,
+        mode: "usecase",
+        name,
+        httpVerb
+      });
     } else if (mode === "adapter") {
-
     } else if (mode === "schema") {
-
     }
   }
 }
