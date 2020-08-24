4d62ad48323fd7f05ae27ee6a88010b8b8e1c935
eyousefifar <eyousefifar@gmail.com>
2020-08-24 19:26:08 +0430

    remove interface export

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export { IValidDirectory } from "./dir";
 export { ISetPackage, TLayers, IPackageMode } from "./packageManager";
-export { IDbClient, IAdd, IPackage } from "./db";
+export { IDbClient, IAdd, IPackage, IRemove } from "./db";
 export {
   ICreateLib,
   IError,
