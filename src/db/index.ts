211018a963a1c135557b5ec4040706e235448139
eyousefifar <eyousefifar@gmail.com>
2020-08-24 19:25:15 +0430

    added remove commnad

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,22 +3,23 @@ import * as nedb from "nedb";
 import { switchToRoot } from "../utils";
 
 import { buildAdd } from "./add";
+import { buildRemove } from "./remove";
 import { buildFindById } from "./findById";
 
 export async function db(serviceName: string) {
   switchToRoot();
-  
+
   const db = new nedb({ filename: `./${serviceName}Db`, autoload: true });
 
   const add = buildAdd({ db });
   const findById = buildFindById({ db });
+  const remove = buildRemove({ db });
   return {
     add,
     findById,
+    remove
   };
 }
-
-
 
 // (async () => {
 //   const actions = await db("user");
@@ -35,5 +36,5 @@ export async function db(serviceName: string) {
 //   const found = await actions.findById(id);
 //   console.log('found');
 //   console.log(found)
- 
+
 // })();
