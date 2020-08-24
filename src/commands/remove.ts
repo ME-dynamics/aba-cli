fa7f194476a9e6973f62e75a728aff5c39963ea5
eyousefifar <eyousefifar@gmail.com>
2020-08-24 19:25:05 +0430

    remove package

--- src/commands/remove.ts
+++ src/commands/remove.ts
@@ -1,32 +1,33 @@
-import {Command } from '@oclif/command'
-import { cli } from 'cli-ux';
-import { exec } from 'child_process'
+import { Command } from "@oclif/command";
+import { cli } from "cli-ux";
+import { exec } from "child_process";
 export default class Remove extends Command {
-  static description = 'remove package from nca'
+  static description = "remove package from your nca project";
 
   static args = [
     {
       name: "packageName",
       required: true,
-      description: "name of js module you want to remove from NCA",
+      description:
+        "name of npm module you want to remove from your nca project",
     },
   ];
 
   async run() {
-    const {args } = this.parse(Remove)
+    const { args } = this.parse(Remove);
     const { packageName } = args;
-    let command:string = `yarn remove ${packageName}`;
-
+    const command = `yarn remove ${packageName}`;
+    // TODO: remove types as well
     cli.action.start(`removing package ${packageName}`);
     try {
-      await(exec(command, (error:any, stdout:any, stdin:any) => {
-        if(error) this.log(error);
+      exec(command, (error: any, stdout: any, stdin: any) => {
+        if (error) this.log(error);
         this.log(stdout);
-      }))
+      });
     } catch (error) {
+      cli.action.stop("remove failed");
       this.error(error);
-      cli.action.stop('remove failed');
     }
-    cli.action.stop('done');
+    cli.action.stop("done");
   }
 }
