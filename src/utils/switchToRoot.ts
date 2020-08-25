9b9ef9510eabc2081ad8511f7c2e658c7370bbcb
eyousefifar <eyousefifar@gmail.com>
2020-08-25 18:07:05 +0430

    support node lib

--- src/utils/switchToRoot.ts
+++ src/utils/switchToRoot.ts
@@ -1,5 +1,6 @@
 import { isValidDirectory } from '../dir';
 import { join } from 'path';
+import { TLibraries } from '../types';
 
 
 
@@ -7,14 +8,14 @@ import { join } from 'path';
 export function switchToRoot(): void {
     const validDir = isValidDirectory();
     // check if project is valid
-    if (!validDir.isValid) throw new Error("not in nca project");
+    if (!validDir.isValid) throw new Error(`not a nca / nodelib/ rrn project`);
   
     // if base is in root, no need to change directory
     if (validDir.base === "root") return;
   
     const currentPath = process.cwd();
     // if base is in packages, should go one level up
-    if (validDir.base === "packages") {
+    if (validDir.base === "packages" || validDir.base === "src") {
       const upDir = join(currentPath, "..");
       process.chdir(upDir);
       return;
@@ -24,8 +25,8 @@ export function switchToRoot(): void {
     // until it reaches root base
     if (validDir.base === "downTheRoad") {
       let upDir: string = currentPath;
-      // 3, because three level of structure is allowed from root base
-      for (let index = 0; index < 3; index++) {
+      // 4, because four level of structure is allowed from root base
+      for (let index = 0; index < 4; index++) {
         upDir = join(upDir, "..");
         process.chdir(upDir);
         if (isValidDirectory().base === "root") {
