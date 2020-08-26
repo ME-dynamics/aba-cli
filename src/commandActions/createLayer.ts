764bf38c0fa39289325d91abd9dc9e2e8353cfc0
eyousefifar <eyousefifar@gmail.com>
2020-08-26 19:54:10 +0430

    final fix for creating layer bug fixed adding layer if you are down the road in nca project

--- src/commandActions/createLayer.ts
+++ src/commandActions/createLayer.ts
@@ -15,25 +15,39 @@ function pathToLayer(
   error: TError,
   layer: TLayers
 ): string {
+  /**
+   * save current directory because after running function one time,
+   * directory will change but isValidDir will show previous and correct results
+   * so second run will result in isValidDir.base ===> downThRoad, but in reality
+   * it is in packages, so it goes up and throw error   
+   */
+  const currentDir = process.cwd();
   let path = "";
   if (isValidDir.base === "root") {
-    path = join(process.cwd(), "packages", layer);
+    path = join(currentDir, "packages", layer);
+    return path;
   } else if (isValidDir.base === "packages") {
-    path = join(process.cwd(), layer);
+    path = join(currentDir, layer);
+    return path;
   } else if (isValidDir.base === "downTheRoad") {
-    let upperPath = process.cwd();
+    let upperPath = currentDir;
     for (let index = 0; index < 4; index++) {
       upperPath = join(upperPath, "..");
       process.chdir(upperPath);
       const checkUpperDir = isValidDirectory();
       if (checkUpperDir.base === "packages") {
         path = join(upperPath, layer);
-        break;
+        process.chdir(currentDir);
+        return path;
       } 
     }
-    error({err: "you should be at most four layers deep in nca projects"})
+    throw error({err: "you should be at most four layers deep in nca projects"})
+    
+  } else {
+    throw error({err:"should be in nca project" })
+    
   }
-  return path;
+  
 }
 
 export function createLayer(args: ICreateLayer) {
