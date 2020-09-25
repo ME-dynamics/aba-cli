6062a24aa0fd214ab1e11557f4db4b6256b0b6f3
eyousefifar <eyousefifar@gmail.com>
2020-09-25 14:40:19 +0330

    export terminateWithError

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { unzip } from "./unzip";
 export { exclusiveFlag } from "./exclusiveFlag";
 export { switchToRoot } from "./switchToRoot";
 export { readPackageJson } from "./readPackageJson";
+export { terminateWithError } from "./terminateWithError";
