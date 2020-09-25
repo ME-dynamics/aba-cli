acf36402fe89baeb482cafa0d1d1590c1cab1909
eyousefifar <eyousefifar@gmail.com>
2020-09-25 20:43:47 +0330

    rewrite using ora, inquirer and some refactor

--- src/commandActions/createLayer.ts
+++ src/commandActions/createLayer.ts
@@ -1,99 +1,69 @@
 import { writeFileSync } from "fs-extra";
-import { cli } from "cli-ux";
+import ora from "ora";
+import { prompt } from "inquirer";
 import {
   controllerLayer,
   entityLayer,
   interfaceLayer,
   usecaseLayer,
-} from "./layerTemplates";
-import { ICreateLayer, IValidDirectory, TError, TLayers } from "../types";
-import { isValidDirectory } from "../dir";
-import { join } from "path";
+} from "../template";
+import { pathToLayer } from "../dir";
+import { terminateWithError } from "../utils";
+import { ICreateLayer } from "../types";
 
-function pathToLayer(
-  isValidDir: IValidDirectory,
-  error: TError,
-  layer: TLayers
-): string {
-  /**
-   * save current directory because after running function one time,
-   * directory will change but isValidDir will show previous and correct results
-   * so second run will result in isValidDir.base ===> downThRoad, but in reality
-   * it is in packages, so it goes up and throw error   
-   */
-  const currentDir = process.cwd();
-  let path = "";
-  if (isValidDir.base === "root") {
-    path = join(currentDir, "packages", layer);
-    return path;
-  } else if (isValidDir.base === "packages") {
-    path = join(currentDir, layer);
-    return path;
-  } else if (isValidDir.base === "downTheRoad") {
-    let upperPath = currentDir;
-    for (let index = 0; index < 4; index++) {
-      upperPath = join(upperPath, "..");
-      process.chdir(upperPath);
-      const checkUpperDir = isValidDirectory();
-      if (checkUpperDir.base === "packages") {
-        path = join(upperPath, layer);
-        process.chdir(currentDir);
-        return path;
-      } 
-    }
-    throw error({err: "you should be at most four layers deep in nca projects"})
-    
-  } else {
-    throw error({err:"should be in nca project" })
-    
-  }
-  
-}
-
-export function createLayer(args: ICreateLayer) {
-  const { error, log, mode, name, httpVerb } = args;
-  const isValidDir = isValidDirectory();
-  if (!isValidDir.isValid) {
-    error({
-      err:
-        "not a valid node clean architecture project, are you sure you're in nca directory",
-    });
-    return;
-  }
-  cli.action.start(`creating ${mode}: ${name}`);
-  if (mode === "entity") {
+export async function createLayer(args: ICreateLayer) {
+  const { mode, name } = args;
+  const layerPath = pathToLayer(mode);
+  const spinner = ora(`${mode}: creating layer ${name}`);
+  if (mode === "entities") {
+    spinner.start();
     const entityFile = entityLayer(name);
-    let entityPath = pathToLayer(isValidDir, error, "entities");
     try {
-      writeFileSync(`${entityPath}/${name}.ts`, entityFile);
-    } catch (err) {
-   
-      error({err: err});
+      writeFileSync(`${layerPath}/${name}.ts`, entityFile);
+      spinner.succeed(`${mode}: layer ${name} created`);
+    } catch (error) {
+      terminateWithError(error, error.exitCode);
     }
-  } else if (mode === "usecase") {
-    if (!httpVerb)
-      throw new Error("when creating use case, http verb must be defined");
-    const usecase = usecaseLayer(name);
-    const controller = controllerLayer(usecase.usecaseName, httpVerb);
-    const interfaceFile = interfaceLayer(
-      controller.controllerName,
-      usecase.usecaseName
-    );
-    const usecasePath = pathToLayer(isValidDir, error, "usecases");
-    const controllerPath = pathToLayer(isValidDir, error, "controllers");
-    const interfacePath = pathToLayer(isValidDir, error, "interfaces");
+  } else if (mode === "usecases") {
+    spinner.info();
+    const { httpVerb } = await prompt([
+      {
+        name: "httpVerb",
+        message: "what method will this usecase respond to?",
+        type: "list",
+        choices: [
+          "GET",
+          "HEAD",
+          "POST",
+          "PUT",
+          "PATCH",
+          "DELETE",
+          "OPTIONS",
+          "CONNECT",
+          "TRACE",
+          "EVENT",
+        ],
+      },
+    ]);
+    const usecaseFile = usecaseLayer(name);
+    const { controllerFile, controllerName } = controllerLayer(name, httpVerb);
+    const interfaceFile = interfaceLayer(controllerName, name);
+    const usecasePath = pathToLayer("usecases");
+    const controllerPath = pathToLayer("controllers");
+    const interfacePath = pathToLayer("interfaces");
     try {
-      writeFileSync(`${usecasePath}/${name}.ts`, usecase.usecaseFile);
-      writeFileSync(`${controllerPath}/${controller.controllerName}.ts`, controller.controllerFile);
+      writeFileSync(`${usecasePath}/${name}.ts`, usecaseFile);
+      writeFileSync(`${controllerPath}/${controllerName}.ts`, controllerFile);
       writeFileSync(`${interfacePath}/${name}.ts`, interfaceFile);
-      cli.action.stop(`${name} created`);
-    } catch (err) {
-      error({err});
+      spinner.succeed(`${mode}: layer ${name} created`);
+    } catch (error) {
+      spinner.fail(`${mode}: failed to create layer ${name}`);
+      terminateWithError(error, error.exitCode);
     }
-  } else if (mode === "adapter") {
+  } else if (mode === "adapters") {
     // TODO: implement later, for db, events, grpc etc
     // INFO: these driver mey require libraries so will complete in time
-  } else if (mode === "schema") {
+  } else if (mode === "schemas") {
     // TODO: implement later
   }
 }
