9f30c9dc03987622a779ffaa7d23466c61192c53
eyousefifar <eyousefifar@gmail.com>
2020-09-26 15:25:22 +0330

    use ErrorFactory

--- src/dir/createDir.ts
+++ src/dir/createDir.ts
@@ -1,15 +1,24 @@
-import { join } from 'path';
-import { mkdirSync } from 'fs-extra';
+import { join } from "path";
+import { mkdirSync } from "fs-extra";
+import { ErrorFactory } from "aba-utils";
 
 /**
  * creates service directory
  */
 
 export async function createDir(serviceName: string) {
-    try {
-      const dir = join(process.cwd(), serviceName);
-      mkdirSync(dir);
-    } catch (error) {
-      throw error; // TODO: use error factory
-    }
+  const currentDir = process.cwd();
+  try {
+    const dir = join(currentDir, serviceName);
+    mkdirSync(dir);
+  } catch (error) {
+    throw new ErrorFactory({
+      name: "fsError",
+      message: "unable to create directory",
+      detail: `unable to create ${serviceName} directory in ${currentDir}`,
+      nativeError: error,
+      path: undefined,
+      timestamp: undefined
+    });
   }
+}
