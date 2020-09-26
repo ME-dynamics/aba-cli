8776a896e2c92105dff9cf9d70bc4b356eab86cc
eyousefifar <eyousefifar@gmail.com>
2020-09-26 15:25:47 +0330

    export new modules

--- src/dir/index.ts
+++ src/dir/index.ts
@@ -1,3 +1,7 @@
 export { isValidDirectory } from "./isValidDirectory";
 export { createDir } from "./createDir";
 export { moveTemplateFiles } from './moveTemplateFiles';
+export { isPackagesDir } from "./isPackagesDir";
+export { isRootDir } from "./isRootDir";
+export { traverseUpValidation } from "./traverseUpValidation";
+export { pathToLayer } from "./pathToLayer";
