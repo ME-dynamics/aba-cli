4f8feb1a2c34f331cc7452635b29db863256f904
eyousefifar <eyousefifar@gmail.com>
2020-09-26 15:28:45 +0330

    more comments, using ErrorFactory

--- src/dir/moveTemplateFiles.ts
+++ src/dir/moveTemplateFiles.ts
@@ -1,9 +1,10 @@
 import { join } from "path";
 import { copy, remove } from "fs-extra";
 import { TLibraries } from "../types";
+import { ErrorFactory } from 'aba-utils';
 /**
- * cause files from github are in folder named your project master
- * when you download from github, this will move it's files a level up
+ * because files from github are in folder named your-project-master
+ * when downloaded from github, this will move it's files a level up
  * to fix this extra folder
  */
 
@@ -17,9 +18,20 @@ export async function moveTemplateFiles(serviceName: string, mode: TLibraries) {
       : "rrn-template-master";
   const path = join(rootPath, dir);
   try {
-    await copy(path, rootPath, { recursive: true, dereference: true });
+    await copy(path, rootPath, { dereference: true, recursive: true });
+    // struggle to get move working, copy works just fine
     await remove(path);
   } catch (error) {
-    throw error; // TODO: use error factory
+    throw new ErrorFactory({
+      name: "fsError",
+      message: "unable to move template file to root folder",
+      detail: "",
+      nativeError: error,
+      path: process.cwd(),
+      timestamp: undefined
+    })
   }
 }
+
+
+
